324de36a515da256d841ca86a3422c1a9c590667
Jan Vansteenkiste <jan@vstone.eu>
2011-05-09 22:42:37 +0200

    Layed out the basics for the javascript

--- multicomplete.js
+++ multicomplete.js
@@ -1,7 +1,27 @@
 (function ($) {
 
+//@todo: Find a way to hook this into drupal without patching drupal core. :/
+
 //@todo: implement generic handling of our KICKASS multicomplete callbacks.
 
-  
+  Drupal.behaviours.multicompleteHandler = {
+    attach: function(context,settings) {
+      $('input.multicomplete', context).bind('', function(event, element) {
+        var form = $(element).closest('form');
+        
+        //@todo: collect input values
+        
+        //@todo: update input fields
+        
+        $.each(values, function(key, value) {
+          if (value.length > 0) {
+            infields[key].val(value);
+          }
+        });
+        
+      })
+    }
+  }
+
 })(jQuery);
 
